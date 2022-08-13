06fe780fea14d280cf58f253734b3bfe5b86288d
MacDantas <marcelodecarvalhodantas@hotmail.com>
2022-08-13 18:07:47 -0300

    Updated obstacles class - Game stop when crashed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,7 @@ const myGameArea = {
   frames: 0,
   obstacles: [],
   balls: [],
+  stop: false,
   start: function () {
     this.canvas.width = 480;
     this.canvas.height = 270;
@@ -36,83 +37,122 @@ class Component {
     this.x += this.speedX;
     this.y += this.speedY;
   }
+
+  top() {
+    return this.y;
+  }
+
+  bottom() {
+    return this.y + this.width;
+  }
+
+  left() {
+    return this.x;
+  }
+
+  rigth() {
+    return this.x + this.height;
+  }
+
+  crashWith(obstacle) {
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.rigth() < obstacle.left() ||
+      this.left() > obstacle.rigth()
+    );
+  }
 }
 
-function createObstacle () {
-    let y = myGameArea.canvas.height
-    let height = 30
-    let width = 30
-    let randomX = Math.floor(Math.random() * (myGameArea.canvas.width - width))
-    let newObstacle = new Component (randomX, 0, width, height, 'red')
-    myGameArea.obstacles.push(newObstacle)
+function createObstacle() {
+  let y = myGameArea.canvas.height;
+  let height = 30;
+  let width = 30;
+  let randomX = Math.floor(Math.random() * (myGameArea.canvas.width - width));
+  let newObstacle = new Component(randomX, 0, width, height, "red");
+  myGameArea.obstacles.push(newObstacle);
 }
 
-
-function createBall () {
-    let y = myGameArea.canvas.height
-    let height = 30
-    let width = 30
-    let randomX = Math.floor(Math.random() * (myGameArea.canvas.width - width))
-    let newBall = new Component (randomX, 0, width, height, 'blue')
-    myGameArea.balls.push(newBall)
+function createBall() {
+  let y = myGameArea.canvas.height;
+  let height = 30;
+  let width = 30;
+  let randomX = Math.floor(Math.random() * (myGameArea.canvas.width - width));
+  let newBall = new Component(randomX, 0, width, height, "blue");
+  myGameArea.balls.push(newBall);
 }
 
-function updateBall (){
-    if (myGameArea.frames % 120 === 0){
-        createBall()
-    }
+function updateBall() {
+  if (myGameArea.frames % 120 === 0) {
+    createBall();
+  }
 
-    for (ball of myGameArea.balls){
-        ball.y += 1
-        ball.draw();
-    }
+  for (ball of myGameArea.balls) {
+    ball.y += 1;
+    ball.draw();
+  }
 }
 
-function updateObstacle (){
-    if (myGameArea.frames % 120 === 0){
-        createObstacle()
-    }
+function updateObstacle() {
+  if (myGameArea.frames % 120 === 0) {
+    createObstacle();
+  }
 
-    for (obstacle of myGameArea.obstacles){
-        obstacle.y += 1
-        obstacle.draw ();
-    }
+  for (obstacle of myGameArea.obstacles) {
+    obstacle.y += 1;
+    obstacle.draw();
+  }
 
-   // myGameArea.obstacles = myGameArea.obstacles.filter (obstacle => obstacle.x > 0 + obstacle.width)
+  myGameArea.obstacles = myGameArea.obstacles.filter(
+    (obstacle) => obstacle.x > 0 + obstacle.width
+  );
+  myGameArea.balls = myGameArea.balls.filter((ball) => ball.x > 0 + ball.width);
+}
+
+function checkGameOver() {
+  const crashed = myGameArea.obstacles.some((obstacle) =>
+    player.crashWith(obstacle)
+  );
+  if (crashed) {
+    myGameArea.stop = true;
+  }
 }
 
 const player = new Component(240, 240, 30, 30, "green");
 
 function updateGameArea() {
-  myGameArea.clear()
-  player.newPos()
-  player.draw()
-  updateObstacle()
-  updateBall()
+  myGameArea.clear();
+  player.newPos();
+  player.draw();
+  updateObstacle();
+  updateBall();
 
-myGameArea.frames += 1
-  requestAnimationFrame(updateGameArea);
+  myGameArea.frames += 1;
+  checkGameOver();
+  if (!myGameArea.stop) {
+    requestAnimationFrame(updateGameArea);
+  }
 }
 
 document.addEventListener("keydown", (e) => {
   const key = e.code;
   switch (key) {
-    case 'ArrowUp': // up arrow
+    case "ArrowUp":
       player.speedY -= 1;
       break;
-    case 'ArrowDown': // down arrow
+    case "ArrowDown":
       player.speedY += 1;
       break;
-    case 'ArrowLeft': // left arrow
+    case "ArrowLeft":
       player.speedX -= 1;
       break;
-    case 'ArrowRight': // right arrow
+    case "ArrowRight":
       player.speedX += 1;
       break;
   }
 });
 
-document.addEventListener('keyup', () => {
-    player.speedX = 0
-    player.speedY = 0
-})
+document.addEventListener("keyup", () => {
+  player.speedX = 0;
+  player.speedY = 0;
+});
